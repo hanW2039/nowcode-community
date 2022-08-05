0f37f141d9c3215f5afa86b0cb88a9d28edb712a
hanw <wasyx1231@outlook.con>
2022-08-05 14:25:33 +0800

    feat(Service):账户修改功能

--- src/main/java/com/hanw/community/service/UserService.java
+++ src/main/java/com/hanw/community/service/UserService.java
@@ -158,4 +158,13 @@ public class UserService implements CommunityConstant {
     public void logout(String ticket){
         loginTicketMapper.updateStatus(ticket,1);
     }
+
+    //查询凭证
+    public LoginTicket findLoginTicket(String ticket){
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
+    public int updateHeader(int userId,String headerUrl){
+        return userMapper.updateHeader(userId,headerUrl);
+    }
 }
